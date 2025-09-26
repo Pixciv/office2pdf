66b12ee30e31f82f6c414b36ae072644100d88dd
Pixciv <snackzmind@gmail.com>
2025-09-26 23:12:51 +0300

    Update build-libreoffice.sh

--- scripts/build-libreoffice.sh
+++ scripts/build-libreoffice.sh
@@ -5,16 +5,31 @@ export PATH=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
 
 cd $LIBO_ROOT
 
-# Cross compile için autogen.input oluştur
-cat <<EOF > autogen.input
---with-distro=CPAndroidAarch64
---build=x86_64-unknown-linux-gnu
---host=aarch64-linux-android
---with-android-ndk=$ANDROID_NDK
---with-android-sdk=$ANDROID_SDK
---enable-sal-log
---enable-debug
-EOF
+# Cross compile için autogen.input benzeri parametreler
+./autogen.sh \
+  --host=aarch64-linux-android \
+  --build=x86_64-unknown-linux-gnu \
+  --with-android-ndk=$ANDROID_NDK \
+  --with-android-sdk=$ANDROID_SDK \
+  --enable-sal-log \
+  --enable-debug \
+  --without-export-validation \
+  --without-helppack-integration \
+  --without-junit \
+  --with-theme=colibre \
+  --disable-poppler \
+  --disable-odk \
+  --enable-android-lok \
+  --with-android-api-level=21 \
+  --with-android-package-name=com.collabora.libreoffice \
+  --with-vendor=Collabora \
+  --disable-scripting-beanshell \
+  --disable-scripting-javascript \
+  --with-lang=de,en-US,es,pt-BR \
+  --with-myspell-dicts \
+  --with-docrepair-fonts \
+  --enable-pdfimport \
+  --enable-release-build \
+  --enable-option-checking=fatal
 
-./autogen.sh
 make -j$(nproc)
