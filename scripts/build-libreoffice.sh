54e09509d3c9766883ae71cae4ff4e9ab6ccec0e
Pixciv <snackzmind@gmail.com>
2025-09-26 23:08:24 +0300

    Update build-libreoffice.sh

--- scripts/build-libreoffice.sh
+++ scripts/build-libreoffice.sh
@@ -2,17 +2,19 @@
 set -e
 
 export PATH=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+
 cd $LIBO_ROOT
 
-# Cross compile için autogen.input benzeri parametreler
-./autogen.sh \
-  --host=aarch64-linux-android \
-  --build=x86_64-unknown-linux-gnu \
-  --with-android-ndk=$ANDROID_NDK \
-  --with-android-api-level=21 \
-  --enable-debug \
-  --disable-poppler \
-  --with-theme=colibre \
-  --enable-sal-log
+# Cross compile için autogen.input oluştur
+cat <<EOF > autogen.input
+--with-distro=CPAndroidAarch64
+--build=x86_64-unknown-linux-gnu
+--host=aarch64-linux-android
+--with-android-ndk=$ANDROID_NDK
+--with-android-sdk=$ANDROID_SDK
+--enable-sal-log
+--enable-debug
+EOF
 
+./autogen.sh
 make -j$(nproc)
