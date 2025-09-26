a6fbf04271e58318a16cbeaef69812610984e571
Pixciv <snackzmind@gmail.com>
2025-09-26 22:02:21 +0300

    Create build-libreoffice.sh

---
+++ scripts/build-libreoffice.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+export PATH=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+cd $LIBO_ROOT
+
+# Cross compile için autogen.input oluştur
+cat <<EOF > autogen.input
+--with-distro=CPAndroidAarch64
+--build=x86_64-unknown-linux-gnu
+--host=aarch64-linux-android
+--with-android-ndk=$ANDROID_NDK
+--with-android-sdk=$ANDROID_SDK
+--enable-sal-log
+--enable-debug
+EOF
+
+./autogen.sh
+make -j$(nproc)
